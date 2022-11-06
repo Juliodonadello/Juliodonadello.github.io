4f3547ad0f30530eb362c83fdd97a139af098c44
Julio <juliodonadello@gmail.com>
2022-11-06 14:40:56 -0300

    url dinamicas

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django import views
 from django.urls import path
-from .views import render_posts
+from .views import render_posts,post_detail
+
+
+app_name = 'blog'
 
 urlpatterns = [
     path('',render_posts,name='post'),
+    path('<int:post_id>',post_detail)
 ]
 
