eb1719b6219c1e5c8f54c98613cd254fd3706f22
Julio <juliodonadello@gmail.com>
2022-11-10 23:21:52 -0300

    styling with booostrap

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import render_posts, post_detail
 app_name = 'blog'
 
 urlpatterns = [
-    path('',render_posts,name='post'),
+    path('',render_posts,name='posts'),
     path('<int:post_id>',post_detail,name='post_detail')
     ]
 
