91859e92cdb1b9d3863421f83f80221f424ab9a8
juliodonadello <juliodonadello@gmail.com>
2024-10-06 21:13:49 -0300

    modificaciones para blog con markdown. TODO: alinear a la izq el campo description

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render,get_object_or_404 
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+import markdown  # Importar la biblioteca markdown
 
 def render_posts(request):
     posts = Post.objects.all()
-    return render(request,'posts.html',{'posts':posts})
+    return render(request, 'posts.html', {'posts': posts})
 
-def post_detail(request,post_id):
+def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request,'post_detail.html', {'post':post})
-
-
+    post.description = markdown.markdown(post.description)  # Convertir Markdown a HTML
+    return render(request, 'post_detail.html', {'post': post})
